ad8f8b795db383c30dc2c8ff67656e2c157144fa
Arvind Vishwakarma <arvindvishwakarma1010@gmail.com>
2026-07-25 02:55:13 +0530

    need to test swagger in prod

--- ExpenseTracker/Program.cs
+++ ExpenseTracker/Program.cs
@@ -29,11 +29,11 @@ namespace ExpenseTracker
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+            //if (app.Environment.IsDevelopment())
+            //{
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+            //}
 
             app.UseHttpsRedirection();
 
